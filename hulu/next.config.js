e6f5b49fcd8aea1cc15b9082eb9ea4801cdda2ec
slimkhemiri <khemiri.medslim1@gmail.com>
2024-11-11 21:08:25 +0100

    deploy

--- hulu/next.config.js
+++ hulu/next.config.js
@@ -1,8 +1,8 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 module.exports = {
   images: {
     domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+  env: {
+    API_KEY: '525c71900b47cfb229d5515e19680392',
   },
 };

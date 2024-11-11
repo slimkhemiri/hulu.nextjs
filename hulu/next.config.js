fd9dc8d65d7206d2e9fb12ddb82473fa80a93061
slimkhemiri <khemiri.medslim1@gmail.com>
2024-11-11 20:23:05 +0100

    solve error

--- hulu/next.config.js
+++ hulu/next.config.js
@@ -1,6 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 module.exports = {
-  images:{
-     domains: ["links.papareact.com","image.tmdb.org"],
-  }
- 
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+};
+
+export const images = {
+  domains: ["links.papareact.com", "image.tmdb.org"],
 };
